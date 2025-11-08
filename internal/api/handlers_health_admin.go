990c3360c046fa2fb87765e960a0d458d6a98ebc
Artexxx <54672403+Artexxx@users.noreply.github.com>
2025-11-08 23:19:16 +0300

    The requirements have changed

--- internal/api/handlers_health_admin.go
+++ internal/api/handlers_health_admin.go
@@ -1,9 +1,16 @@
 package api
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/valyala/fasthttp"
+	"strings"
 )
+
+type resetRequest struct {
+	Password string `json:"password"` // пароль
+}
 
 // @Summary Проверка здоровья сервиса
 // @Tags    Admin
@@ -15,10 +22,26 @@ func (s *Service) healthHandler(ctx *fasthttp.RequestCtx) {
 
 // @Summary Полная очистка данных тренажёра (truncate tables.*)
 // @Tags    Admin
+// @Param   request body resetRequest true "Пароль"
 // @Success 200 {object} okResponse
 // @Failure 500 {object} errorResponse
 // @Router  /admin/reset [post]
 func (s *Service) resetHandler(ctx *fasthttp.RequestCtx) {
+	var req resetRequest
+	if err := json.Unmarshal(ctx.PostBody(), &req); err != nil {
+		writeError(ctx, fasthttp.StatusBadRequest, fmt.Errorf("json.Unmarshal: %w", err))
+		return
+	}
+	if strings.TrimSpace(req.Password) == "" {
+		writeError(ctx, fasthttp.StatusBadRequest, errors.New("required field 'admin_password'"))
+		return
+	}
+
+	if req.Password != "admin89213password" {
+		writeError(ctx, fasthttp.StatusUnauthorized, errors.New("invalid admin password"))
+		return
+	}
+
 	if err := s.events.ResetAll(ctx); err != nil {
 		writeError(ctx, fasthttp.StatusInternalServerError, fmt.Errorf("events.ResetAll: %w", err))
 		return
